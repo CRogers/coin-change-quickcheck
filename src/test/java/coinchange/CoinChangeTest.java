97a0560a11f8babcc5f84ebf6d6e1dbc626c4ef2
Callum Rogers <callumrogers@hotmail.co.uk>
2017-03-05 21:56:05 +0000

    Ensure 10s are produced rather than many twos

--- src/test/java/coinchange/CoinChangeTest.java
+++ src/test/java/coinchange/CoinChangeTest.java
@@ -7,6 +7,7 @@ import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.assertj.core.api.Condition;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -47,7 +48,25 @@ public class CoinChangeTest {
         assertThat(numberOfOnes).isLessThanOrEqualTo(1);
     }
 
+    @Property
+    public void never_have_more_than_a_four_two_pence_coins(
+        @InRange(minInt = 1, maxInt = 600) int total
+    ) {
+        long numberOfOnes = changeFor(total).stream()
+            .filter(Predicate.isEqual(2))
+            .count();
+        assertThat(numberOfOnes).isLessThanOrEqualTo(4);
+    }
+
     private List<Integer> changeFor(int total) {
+        Stream<Integer> tens = Stream
+            .generate(() -> 10)
+            .limit(total / 10);
+        Stream<Integer> leftOver = changeForTwosAndOnes(total % 10).stream();
+        return Stream.concat(tens, leftOver).collect(Collectors.toList());
+    }
+
+    private List<Integer> changeForTwosAndOnes(int total) {
         Stream<Integer> ones = Stream
             .generate(() -> 1)
             .limit(total % 2);
@@ -55,5 +74,10 @@ public class CoinChangeTest {
             .generate(() -> 2)
             .limit(total / 2);
         return Stream.concat(ones, twos).collect(Collectors.toList());
+    }
+
+    private <T> Stream<T> concatAll(Stream<T>... streams) {
+        return Arrays.stream(streams)
+            .reduce(Stream.of(), Stream::concat);
     }
 }
