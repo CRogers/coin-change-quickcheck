dc3f068c2bf70b429841abaca7da7c8760e033c7
Callum Rogers <callumrogers@hotmail.co.uk>
2017-03-05 22:18:46 +0000

    Remove duplication

--- src/main/java/coinchange/CoinChanger.java
+++ src/main/java/coinchange/CoinChanger.java
@@ -1,33 +1,35 @@
 package coinchange;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CoinChanger {
     public static List<Integer> changeFor(int total) {
-        Stream<Integer> tens = Stream
-            .generate(() -> 10)
-            .limit(total / 10);
-        Stream<Integer> leftOver = changeForFives(total % 10).stream();
-        return Stream.concat(tens, leftOver).collect(Collectors.toList());
+        return blah(total, 10, CoinChanger::changeForFives);
     }
 
     private static List<Integer> changeForFives(int total) {
-        Stream<Integer> fives = Stream
-            .generate(() -> 5)
-            .limit(total / 5);
-        Stream<Integer> leftOvers = changeForTwosAndOnes(total % 5).stream();
-        return Stream.concat(fives, leftOvers).collect(Collectors.toList());
+        return blah(total, 5, CoinChanger::changeForTwos);
     }
 
-    private static List<Integer> changeForTwosAndOnes(int total) {
-        Stream<Integer> ones = Stream
+    private static List<Integer> changeForTwos(int total) {
+        return blah(total, 2, CoinChanger::changeForOnes);
+    }
+
+    private static List<Integer> blah(int total, int coin, Function<Integer, List<Integer>> func) {
+        Stream<Integer> tens = Stream
+            .generate(() -> coin)
+            .limit(total / coin);
+        Stream<Integer> leftOver = func.apply(total % coin).stream();
+        return Stream.concat(tens, leftOver).collect(Collectors.toList());
+    }
+
+    private static List<Integer> changeForOnes(int total) {
+        return Stream
             .generate(() -> 1)
-            .limit(total % 2);
-        Stream<Integer> twos = Stream
-            .generate(() -> 2)
-            .limit(total / 2);
-        return Stream.concat(ones, twos).collect(Collectors.toList());
+            .limit(total)
+            .collect(Collectors.toList());
     }
 }
