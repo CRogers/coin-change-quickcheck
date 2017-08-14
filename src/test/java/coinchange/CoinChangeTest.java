ce10e11de738ef149b76b470619d89e461edcd45
Callum Rogers <callumr@palantir.com>
2017-08-14 10:46:19 +0100

    Add valid coins

--- src/test/java/coinchange/CoinChangeTest.java
+++ src/test/java/coinchange/CoinChangeTest.java
@@ -1,5 +1,6 @@
 package coinchange;
 
+import com.google.common.collect.ImmutableList;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
@@ -11,6 +12,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnitQuickcheck.class)
 public class CoinChangeTest {
+    private final List<Integer> validCoins = ImmutableList.of(
+            1, 2, 5, 10, 20, 50, 100, 200
+    );
+
     @Property
     public void the_coins_of_the_change_given_should_total_the_change_asked_for(
         @InRange(minInt = 1, maxInt = 600) int total
